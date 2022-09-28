8410c233389d70471487a5bce2186a46f9219a82
s-blu <git@s-blu.de>
2022-09-28 11:08:27 +0200

    chore(build): add missing ,

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "."
+  base: "./",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
